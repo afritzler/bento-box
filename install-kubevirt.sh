c23c5334e62106ad68e76cc97042a4e5b4d0e160
Andreas Fritzler <andreas.fritzler@gmail.com>
2019-03-20 13:31:34 +0100

    Restructured manifest folder

--- install-kubevirt.sh
+++ install-kubevirt.sh
@@ -3,5 +3,5 @@
 export VERSION=v0.15.0
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-operator.yaml
 # enable emulation mode for libVirt
-kubectl apply -f manifests/kubevirt-cm.yaml
+kubectl apply -f manifests/kubevirt/kubevirt-cm.yaml
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-cr.yaml
