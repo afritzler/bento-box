c356307db82b007c84ab779268f93557012f909f
Andreas Fritzler <andreas.fritzler@gmail.com>
2019-03-22 21:34:47 +0100

    Added initial gardener configuration

---
+++ configure-gardener.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set +e
+
+mkdir -p ./gen
+
+# put the manifests in place
+cp ./manifests/gardener-config/* ./gen
+cp ./manifests/templates/seed.yaml ./gen/50-seed.yaml
+cp ./manifests/templates/seed-secret.yaml.template ./gen/40-seed-secret.yaml
+cp ./manifests/templates/core-secret.yaml.template ./gen/70-core-secret.yaml
+
+kubeconfig="$(base64 -w 0 ./kubeconfig)"
+ingressip="$(kubectl -n nginx-ingress get svc nginx-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+
+os="$(uname -s)"
+case "${os}" in
+    Linux*)  sed -i 's/INGRESSIP/'"$ingressip"'/g' ./gen/50-seed.yaml
+             sed -i 's/KUBECONFIG/'"$kubeconfig"'/g' ./gen/40-seed-secret.yaml
+             sed -i 's/KUBECONFIG/'"$kubeconfig"'/g' ./gen/70-core-secret.yaml;;
+    Darwin*) sed -i 's/INGRESSIP/'"$ingressip"'/g' ./gen/50-seed.yaml
+             sed -i '' 's/KUBECONFIG/'"$kubeconfig"'/g' ./gen/40-seed-secret.yaml
+             sed -i '' 's/KUBECONFIG/'"$kubeconfig"'/g' ./gen/70-core-secret.yaml;;
+    *)       echo "Not supported Operating System ${unameOut}"
+esac
+
+kubectl apply -f ./gen/
--- install-kubevirt.sh
+++ install-kubevirt.sh
@@ -4,4 +4,5 @@ export VERSION=v0.15.0
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-operator.yaml
 # enable emulation mode for libVirt
 kubectl apply -f manifests/kubevirt/kubevirt-cm.yaml
+sleep 5
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-cr.yaml
