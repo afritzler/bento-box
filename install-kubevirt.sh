676174ff297da72887cc4506150b73cb14ee5ca0
Andreas Fritzler <andreas.fritzler@gmail.com>
2019-03-26 23:23:58 +0100

    Added mutation webhook and fixed gardener configuration

--- install-kubevirt.sh
+++ install-kubevirt.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export VERSION=v0.15.0
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-operator.yaml
+#kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-operator.yaml
+kubectl apply -f manifests/kubevirt/kubevirt-operator.yaml
 # enable emulation mode for libVirt
 kubectl apply -f manifests/kubevirt/kubevirt-cm.yaml
 sleep 5
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-cr.yaml
+
+kubectl create ns pool
